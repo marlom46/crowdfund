878f3bc24d08a1c596ad6f2b98f3c6198ac5b946
marlom46 <80200977+marlom46@users.noreply.github.com>
2021-07-25 22:38:10 +0530

    Update campaign.test.js

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -37,6 +37,69 @@ describe('Camgaign', () => {
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
     });
+
+    it('marks caller as the campaign manager', async () => {
+
+        const manager = await campaign.methods.manager().call();
+        assert.strictEqual(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and add them as approvers', async () => {
+
+        await campaign.methods.contribute().send({
+
+            value: '200',
+            from: accounts[1]
+        });
+
+        const isContributer = await campaign.methods.approvers(accounts[1]).call();
+        assert.ok(isContributer);
+    });
+
+    it('requires a min. contribution', async () => {
+
+        try {
+
+            await campaign.methods.contribute().send({
+
+                value: '5',
+                from: accounts[1]
+            });
+
+            assert(false);
+
+        } catch (error) {
+
+            assert(error);
+        }
+    });
+
+    it('allows manager to make payment request', async () => {
+
+        await campaign.methods
+            .createRequest('Buy screens', '100', accounts[1]).send({
+
+                from: accounts[0],
+                gas: '1000000'
+            });
+
+        const request = await campaign.methods.requests(0).call();
+
+        assert.strictEqual('Buy screens', request.description);
+    });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
